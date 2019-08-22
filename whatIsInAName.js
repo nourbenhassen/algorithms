3679ab1a7d5c36ad0d082562d89a7fef5484e21f
Nour <nour.benhassen@gmail.com>
2019-08-22 19:08:55 +0200

    create whatIsInAName.js

---
+++ whatIsInAName.js
@@ -0,0 +1,49 @@
+/**
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
+*/
+
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  let a = 0;
+  // Only change code below this line
+  for (let i=0; i<collection.length; i++) { // object in collection
+    a=0;
+      for (let j in source) { //key in source
+        for (let k in collection[i]) { //key in object of collection
+          //console.log('source', source[j]);
+          //console.log('collection', collection[i][k]);
+            if (collection[i][k] == source[j] && j == k){ 
+              a++;
+              }
+
+             if (a == Object.keys(source).length) {
+               arr.push(collection[i]);
+               a = 0;
+             }
+        }
+      }
+  }
+
+  
+  // Only change code above this line
+  return arr;
+}
+
+console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }));
+
+/** TESTS
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }) should return [{ first: "Tybalt", last: "Capulet" }].
+Passed
+whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }) should return [{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }].
+Passed
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }) should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }].
+Passed
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }) should return [{ "apple": 1, "bat": 2, "cookie": 2 }].
+Passed
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat":2 }], { "apple": 1, "bat": 2 }) should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie":2 }].
+Passed
+whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3}) should return []
+*/
